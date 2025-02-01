e84de9bf533cb41a998a6ebde6e5aa1c94c5efe4
him16112 <99254194+him16112@users.noreply.github.com>
2025-02-01 16:19:12 +0530

    Update index.js

--- onebloodbackend/index.js
+++ onebloodbackend/index.js
@@ -25,7 +25,7 @@ const gmailAppPassword = process.env.GMAIL_APP_PASSWORD;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     methods: "POST, GET, OPTIONS",
     allowedHeaders: ["Content-Type"],
     credentials: true, // Allow credentials
